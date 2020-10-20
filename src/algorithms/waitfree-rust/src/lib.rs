5c1cef36f3fe5487a7dca0455bcd790209aa735e
Banks, Xavier <xavier.xb95@gmail.com>
2020-10-19 22:41:51 -0400

    Adding Vector API and descriptors

--- src/algorithms/waitfree-rust/src/lib.rs
+++ src/algorithms/waitfree-rust/src/lib.rs
@@ -1,7 +1,109 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::rc::Rc;
+use std::sync::Arc;
+
+trait Vector {
+    // API Methods
+    fn push_back(&self, value: usize) -> bool;
+    fn pop_back(&self) -> usize;
+    fn at(&self, index: usize) -> usize;
+    fn insert_at(&self, index: usize, element: usize) -> bool;
+    fn erase_at(&self, index: usize, element: usize) -> bool;
+    fn cwrite(&self, index: usize, element: usize) -> bool;
+
+    fn announce_op(&self, descriptor: dyn Descriptor);
+}
+
+trait Descriptor {
+    fn complete(&self) -> bool;
+    fn value(&self) -> usize;
+}
+
+// PopDescr consists solely of a reference to a PopSubDescr (child) which is initially Null.
+struct PopDescr {
+    vec: Rc<Vector>,
+    pos: usize,
+    child: Arc<PopSubDescr>
+}
+
+// PopSubDescr consists of a reference to a previously placed PopDescr (parent)
+// and the value that was replaced by the PopSubDescr (value).
+struct PopSubDescr {
+    parent: Rc<PopDescr>,
+    value: usize,
+}
+
+enum PushState {
+    Undecided,
+    Failed,
+    Passed,
+}
+
+// contains the value to be pushed and a state member
+struct PushDescr {
+    vec: Rc<Vector>,
+    value: usize,
+    pos: usize,
+    state: PushState
+}
+
+struct ShiftOp {
+    vec: Rc<Vector>,
+    pos: usize,
+    incomplete: bool,
+    next: Arc<ShiftDescr>,
+}
+
+struct ShiftDescr {
+    op: Rc<ShiftOp>,
+    pos: usize,
+    value: usize,
+    prev: Rc<ShiftDescr>,
+    next: Arc<ShiftDescr>,
+}
+
+// Implementations for the different Descriptors
+
+impl Descriptor for PopDescr {
+    fn complete(&self) -> bool {
+        todo!()
+    }
+    fn value(&self) -> usize {
+        todo!()
+    }
+}
+
+impl Descriptor for PopSubDescr {
+    fn complete(&self) -> bool {
+        todo!()
+    }
+    fn value(&self) -> usize {
+        todo!()
+    }
+}
+
+impl Descriptor for PushDescr {
+    fn complete(&self) -> bool {
+        todo!()
+    }
+    fn value(&self) -> usize {
+        todo!()
+    }
+}
+
+impl Descriptor for ShiftOp {
+    fn complete(&self) -> bool {
+        todo!()
+    }
+    fn value(&self) -> usize {
+        todo!()
+    }
+}
+
+impl Descriptor for ShiftDescr {
+    fn complete(&self) -> bool {
+        todo!()
+    }
+    fn value(&self) -> usize {
+        todo!()
     }
 }
